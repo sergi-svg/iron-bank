354f7d45a13bb6a4d68e45cf000328158ab14800
sergi-svg <sergivaque@gmail.com>
2024-04-20 18:01:58 +0200

    Added: -CustomerController -CustomerService -data.sql

--- src/main/java/dev/svg/controllers/CustomerController.java
+++ src/main/java/dev/svg/controllers/CustomerController.java
@@ -2,6 +2,7 @@ package dev.svg.controllers;
 
 import dev.svg.model.Customer;
 import dev.svg.services.CustomerService;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
