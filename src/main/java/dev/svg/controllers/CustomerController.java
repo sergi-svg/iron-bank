7c4a6cd7870de2ac746055d91a741885b5369400
Sergi Vaqué <sergi.vaque@allianz.com>
2024-04-29 20:58:10 +0200

    Post for customers

--- src/main/java/dev/svg/controllers/CustomerController.java
+++ src/main/java/dev/svg/controllers/CustomerController.java
@@ -2,6 +2,7 @@ package dev.svg.controllers;
 
 import dev.svg.model.Customer;
 import dev.svg.services.CustomerService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -43,7 +44,7 @@ public class CustomerController implements ResourceController <Customer> {
 
     @PostMapping("/customers")
     @ResponseStatus(HttpStatus.CREATED)
-    public Customer createResource(Customer customer) {
+    public Customer createResource(@RequestBody @Valid Customer customer) {
         return customerService.createCustomer(customer);
     }
 
